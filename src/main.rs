37207df544137b02ed6c0ea8cff4e89249575bab
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-01-29 01:43:33 +0300

    Add authentication methods and default user/password to proxy

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ mod socks5;
 use std::{error::Error, sync::Arc};
 use tokio;
 
-
-
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let server_addr: String = String::from("0.0.0.0");
@@ -16,11 +13,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     tokio::spawn(async move {
         // new vec of auth methods
         let mut authTypes: Vec<socks5::AuthMethods> = Vec::new();
-        authTypes.push(socks5::AuthMethods::NoAuth);
 
-        let proxy: socks5::Proxy = socks5::Proxy::new(
-            authTypes
-        );
+        // add auth methods
+        authTypes.push(socks5::AuthMethods::NoAuth);
+        authTypes.push(socks5::AuthMethods::UsernamePassword);
+
+        let mut proxy: socks5::Proxy = socks5::Proxy::new(authTypes);
+
+        // Create defoult user and password
+        proxy.add_user(String::from("user"), String::from("pass"));
 
         // Create a new Tokio runtime
         match socks5::start_proxy(
