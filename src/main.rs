cf9e797f46735bd5fe32cafc788b151749fe26af
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2024-02-20 04:11:49 +0300

    Refactor main.rs to use the api module and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,47 @@
 #![allow(dead_code)]
 
+mod api;
 mod proxy;
 mod socks5;
 
 use crate::proxy::Proxy;
 use crate::socks5::libs::statics::AuthMethods;
-use socks5::handlers;
-use std::{error::Error, sync::Arc};
+use api::app::app;
+use socks5::{handlers, libs::statics::FromToU8};
+use std::{io, sync::{Arc, Mutex}};
 use tokio;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let server_addr: String = String::from("0.0.0.0");
+async fn main() -> Result<(), io::Error> {
     let server_port: i32 = 1080;
 
+    let mut proxy: Proxy = proxy::Proxy::new();
+
+    proxy.add_method(AuthMethods::NoAuth.to_u8());
+    proxy.add_method(AuthMethods::UsernamePassword.to_u8());
+
+
+    // Create defoult user and password
+    proxy.add_user(
+        String::from("user"),
+        String::from("pass"),
+    );
+
+    let proxy_arc = Arc::new(proxy);
+    let proxy_arc_clone = proxy_arc.clone();
+
     tokio::spawn(async move {
-        // new vec of auth methods
-        let mut auth_types: Vec<AuthMethods> = Vec::new();
-
-        // add auth methods
-        auth_types.push(AuthMethods::NoAuth);
-        auth_types.push(AuthMethods::UsernamePassword);
-
-        let mut proxy: Proxy =
-            proxy::Proxy::new(auth_types);
-
-        // Create defoult user and password
-        proxy.add_user(
-            String::from("user"),
-            String::from("pass"),
-        );
-
         // Create a new Tokio runtime
         match handlers::start_proxy(
-            Arc::new(proxy),
-            Some(server_addr.clone()),
+            proxy_arc,
             Some(server_port.clone()),
         )
         .await
         {
             socks5::libs::errors::ProxyResult::Err(_e) => {}
-            socks5::libs::errors::ProxyResult::Ok(_n) => {
-                println!(
-                    "Server started on {}:{}",
-                    server_addr, server_port
-                );
-            }
+            socks5::libs::errors::ProxyResult::Ok(_n) => {}
         }
     });
 
-    // Lock
-    loop {}
+    app(proxy_arc_clone).await
 }
